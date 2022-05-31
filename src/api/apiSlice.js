2367b99f8952eb6cde910a49ca8f248cd52466a6
myatniy <ilya.varabei1@gmail.com>
2022-05-31 17:54:31 +0300

    Displaying Individual Posts

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -13,8 +13,11 @@ export const apiSlice = createApi({
       // The URL for the request is '/fakeApi/posts'
       query: () => '/posts',
     }),
+    getPost: builder.query({
+      query: (postId) => `/posts/${postId}`,
+    }),
   }),
 })
 
 // Export the auto-generated hook for the `getPosts` query endpoint
-export const { useGetPostsQuery } = apiSlice
+export const { useGetPostsQuery, useGetPostQuery } = apiSlice
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,34 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
-import { selectPostById } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
+import { useGetPostQuery } from '../../api/apiSlice'
+import { Spinner } from '../../components/Spinner'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
-
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
-        <PostAuthor userId={post.user} />
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
-        <ReactionButtons post={post} />
       </article>
-    </section>
-  )
+    )
+  }
+
+  return <section>{content}</section>
 }
