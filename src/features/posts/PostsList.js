cb66c0f945e971f4fc860c99385e90c6f592648e
myatniy <ilya.varabei1@gmail.com>
2022-05-31 18:27:04 +0300

    Refetching Posts Manually

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,6 +6,7 @@ import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
 import { useGetPostsQuery } from '../../api/apiSlice'
+import classnames from 'classnames'
 
 let PostExcerpt = ({ post }) => {
   return (
@@ -29,9 +30,11 @@ export const PostsList = () => {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
+    refetch,
   } = useGetPostsQuery()
 
   const sortedPosts = useMemo(() => {
@@ -45,9 +48,15 @@ export const PostsList = () => {
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ))
+
+    const containerClassname = classnames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
   } else if (isError) {
     content = <div>{error}</div>
   }
@@ -55,6 +64,7 @@ export const PostsList = () => {
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   )
